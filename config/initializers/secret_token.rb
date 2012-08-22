e8617265b3d3f05333d3028d222e8f29a0b53e43
homakov <homakov@gmail.com>
2012-08-22 20:53:45 +0300

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can generate a safe secret key with the command `rake secret`.
+
+# Make sure your secret_token is kept private
+# if you're sharing your code publicly.
+Charm::Application.config.secret_token = '94b3e14ec7b8e10b0363aa4ea5eef7059bf7edd271f8e5954d4cd745f349f5e5746c09686cf6c7034b6bf591774640017704805a5e30313b6389bcfd1658e7bf'
